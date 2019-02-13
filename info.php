d46055ca4e474423d21d4640ca43eb386697506e
benthud82 <38083671+benthud82@users.noreply.github.com>
2019-02-13 17:58:54 -0600

    Create info.php

---
+++ info.php
@@ -0,0 +1,22 @@
+<!doctype html>
+
+<html>
+<head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-122388040-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-122388040-1');
+</script>
+
+</head>
+
+<?php
+echo 'should be tracking now';
+echo 'that has been updated from a github';
+?>
+
+</html>
